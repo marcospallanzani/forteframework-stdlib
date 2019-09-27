8e7a77a57e038aa2f10928204b5b5a349d27bf02
Marco Spallanzani <marco.spallanzani.ms@gmail.com>
2019-09-27 22:00:20 +0200

    add new WrongParameterException;

---
+++ src/Exceptions/WrongParameterException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Forte\Stdlib\Exceptions;
+
+/**
+ * Class WrongParameterException.
+ *
+ * @package Forte\Stdlib\Exceptions
+ */
+class WrongParameterException extends GeneralException
+{
+    //
+}
