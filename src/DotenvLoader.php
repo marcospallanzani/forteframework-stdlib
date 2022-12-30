b35c48012390718083ba72d1c00b18840beafcb9
Marco <marco.spallanzani.ms@gmail.com>
2022-12-30 20:29:00 +0100

    fix PHPStan errors

--- src/DotenvLoader.php
+++ src/DotenvLoader.php
@@ -13,6 +13,7 @@ declare(strict_types=1);
 
 namespace Forte\Stdlib;
 
+use Dotenv\Exception\InvalidFileException;
 use Dotenv\Parser\Lines;
 use Dotenv\Parser\Parser;
 use Forte\Stdlib\Exceptions\GeneralException;
@@ -31,22 +32,29 @@ class DotenvLoader
      *
      * @param string $filePath The path of the file to be read.
      *
-     * @return array An array representation of the given file.
+     * @return array<string, mixed> An array representation of the given file.
      *
      * @throws GeneralException
      */
-    public static function loadIntoArray(string $filePath)
+    public static function loadIntoArray(string $filePath): array
     {
         // We get the given file content as a list of strings
         $content = self::findAndRead($filePath);
-//TODO check the preg_split second parameter... should it be a string or an array?
         try {
-            // We remove the empty spaces and comments and we return a list
+            // We remove the empty spaces and comments, and we return a list
             // of all env variables found in the file
-            return self::processEntries(
-                Lines::process(preg_split("/(\r\n|\n|\r)/", $content))
-            );
-        } catch (\Dotenv\Exception\InvalidFileException $invalidFileException) {
+            $lines = [];
+
+            $contentLines = \preg_split("/(\r\n|\n|\r)/", $content);
+            if (true === \is_array($contentLines)) {
+                foreach ($contentLines as $line) {
+                    if (true === \is_string($line)) {
+                        $lines[] = $line;
+                    }
+                }
+            }
+            return self::processEntries(Lines::process($lines));
+        } catch (InvalidFileException $invalidFileException) {
             throw new GeneralException(sprintf(
                 "Error occurred while reading the file '%s'. Error message is: %s",
                 $filePath,
@@ -66,7 +74,7 @@ class DotenvLoader
     public static function getLineFromVariables(string $key, string $value): string
     {
         // We need to add double quotes to the value, if it contains spaces
-        if (str_contains($value, ' ')) {
+        if (\str_contains($value, ' ')) {
             $value = '"' . $value . '"';
         }
 
@@ -78,33 +86,31 @@ class DotenvLoader
      *
      * @param string $filePath The path of the file to be read.
      *
-     * @return array<string>
+     * @return string The file content
      *
      * @throws GeneralException Either the file path is empty or it was not
      * possible to read the specified file.
      */
-    protected static function findAndRead(string $filePath)
+    protected static function findAndRead(string $filePath): string
     {
-        if ('' !== $filePath) {
+        if ('' === \trim($filePath)) {
             throw new GeneralException('The file path must be provided.');
         }
 
-        $content = @file_get_contents($filePath);
+        $content = @\file_get_contents($filePath);
         $lines = Option::fromValue($content, false);
+
         if ($lines->isDefined()) {
-            return $lines->get();
+            return (string) $lines->get();
         }
 
         throw new GeneralException(
-            sprintf('Unable to read the environment file [%s].', $filePath)
+            \sprintf('Unable to read the environment file [%s].', $filePath)
         );
     }
 
     /**
      * Process the environment variable entries.
-     *
-     * We'll fill out any nested variables, and actually set the variable using
-     * the underlying environment variables instance.
      *
      * @param array<string> $entries
      *
@@ -114,19 +120,26 @@ class DotenvLoader
     {
         $vars = [];
         foreach ($entries as $entry) {
-            $parsedEntries = (new Parser())->parse($entry);
-            foreach ($parsedEntries as $parsedEntry) {
-                $value = $parsedEntry->getValue();
-                if (is_numeric($value)) {
-                    $value = is_int($value) ? intval($value) : floatval($value);
-                } elseif (is_string($value)) {
-                    if ('false' === strtolower($value)) {
+            $parserEntries = (new Parser())->parse($entry);
+            foreach ($parserEntries as $parserEntry) {
+                $value = $parserEntry->getValue();
+                if ($value instanceof \PhpOption\Some) {
+                    $value = $value->get();
+                    if ($value instanceof \Dotenv\Parser\Value) {
+                        $value = $value->getChars();
+                    }
+                }
+
+                if (\is_numeric($value)) {
+                    $value = \is_int($value) ? $value : \floatval($value);
+                } elseif (\is_string($value)) {
+                    if ('false' === \strtolower($value)) {
                         $value = false;
-                    } elseif ('true' === strtolower($value)) {
+                    } elseif ('true' === \strtolower($value)) {
                         $value = true;
                     }
                 }
-                $vars[$parsedEntry->getName()] = $value;
+                $vars[$parserEntry->getName()] = $value;
             }
         }
         return $vars;
